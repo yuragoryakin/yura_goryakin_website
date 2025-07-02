7fcb5b9d6cc6299e4428d5e94481e371cf820f5d
yuragoryakin <yu.goryakin@gmail.com>
2025-07-02 19:35:02 +0400

    refactor: implement responsive fonts and fix scroll

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -12,38 +12,18 @@ export default function Section({ id, title, children }: SectionProps) {
   return (
     <section
       id={id}
-      className={`flex flex-col w-full min-h-screen scroll-mt-20`}
+      className={`flex flex-col w-full min-h-screen scroll-mt-20 px-6`}
       tabIndex={-1}
     >
       <div className="w-full flex flex-col pt-[240px]">
       {isIntro ? (
-          <h1
-            className="font-semibold text-[72px] font-[Rethink Sans,sans-serif] m-0 p-0 text-black text-left"
-            style={{
-              fontFamily: "Rethink Sans, sans-serif",
-              fontWeight: 600,
-              fontSize: 72,
-              lineHeight: "110%",
-              margin: 0,
-              padding: 0,
-            }}
-            >
-              {title}
-            </h1>
+          <h1 className="font-semibold text-5xl md:text-7xl font-[Rethink Sans,sans-serif] m-0 p-0 text-black text-left">
+            {title}
+          </h1>
         ) : (
-          <div className="text-[18px] font-normal space-y-6 transform -translate-y-9">
+          <div className="text-base md:text-lg font-normal space-y-6 transform -translate-y-9">
             {title && title.length > 0 && (
-            <h2
-                className="font-semibold text-[72px] font-[Rethink Sans,sans-serif] m-0 p-0 text-gray-900 text-left mb-10"
-                style={{
-                  fontFamily: "Rethink Sans, sans-serif",
-                  fontWeight: 600,
-                  fontSize: 72,
-                  lineHeight: "110%",
-                  margin: 0,
-                  padding: 0,
-                }}
-            >
+            <h2 className="font-semibold text-5xl md:text-7xl font-[Rethink Sans,sans-serif] m-0 p-0 text-gray-900 text-left mb-10">
               {title}
             </h2>
           )}
